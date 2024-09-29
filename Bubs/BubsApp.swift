13ecafe95150f2a1b00598cb99f050ecbd072a8e
Hayaalbaker <144670605+Hayaalbaker@users.noreply.github.com>
2024-09-29 17:23:57 +0300

    Initial Commit

---
+++ Bubs/BubsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BubsApp.swift
+//  Bubs
+//
+//  Created by Haya Albaker on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct BubsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BubsTests/BubsTests.swift
@@ -0,0 +1,17 @@
+//
+//  BubsTests.swift
+//  BubsTests
+//
+//  Created by Haya Albaker on 29/09/2024.
+//
+
+import Testing
+@testable import Bubs
+
+struct BubsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
