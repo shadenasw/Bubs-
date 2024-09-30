4d01588a490b74ea91ee094319595533289ac940
ranad101 <aldawoodranad@outlook.com>
2024-09-30 17:43:38 +0300

    first update

--- Bubs/HomePage.swift
+++ Bubs/HomePage.swift
@@ -9,8 +9,23 @@ import SwiftUI
 
 struct HomePage: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("hi")
+        ZStack{
+            Image("IMG_3451")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+        
+            Text("BUBS")
+                .font(.system(size: 70, weight: .bold, design: .rounded))
+                .foregroundStyle(.blue)
+                .padding(.top,-180)
+            
+            Image(systemName: "play.circle.fill")
+                .resizable()
+                .frame(width: 100, height: 100)
+                .foregroundStyle(.blue)
+        }
+        .ignoresSafeArea()
     }
 }
 
