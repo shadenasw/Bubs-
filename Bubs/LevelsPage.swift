d7492288d9e195cc418940c91a9c5017e4745c0c
Wasan Aljohani <wasanalmrwani@hotmail.com>
2024-09-30 07:49:53 -0700

    done

---
+++ Bubs/LevelsPage.swift
@@ -0,0 +1,29 @@
+//
+//  LevelsPage.swift
+//  Bubs
+//
+//  Created by Wasan Aljohani on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct LevelsPage: View {
+    var body: some View {
+        ZStack {
+            Image("sink")
+                .resizable()
+                .scaledToFit()
+                .edgesIgnoringSafeArea(.all)
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            
+            }
+            
+        }
+    }
+
+
+#Preview {
+    LevelsPage()
+}
