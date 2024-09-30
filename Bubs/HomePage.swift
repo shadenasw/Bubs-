540a67f5dcdb554c06f185f417bfb060a0c42415
ranad101 <aldawoodranad@outlook.com>
2024-09-30 16:49:07 +0300

    update

---
+++ Bubs/HomePage.swift
@@ -0,0 +1,19 @@
+//
+//  HomePage.swift
+//  Bubs
+//
+//  Created by Ranad aldawood on 27/03/1446 AH.
+//
+
+import SwiftUI
+
+struct HomePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("hi")
+    }
+}
+
+#Preview {
+    HomePage()
+}
