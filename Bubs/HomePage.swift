2588fe62762420feb5068a27ed48aff39317fdd5
ranad101 <aldawoodranad@outlook.com>
2024-10-01 17:22:53 +0300

    navigation

--- Bubs/HomePage.swift
+++ Bubs/HomePage.swift
@@ -10,39 +10,38 @@ import SwiftUI
 struct HomePage: View {
     @State private var scale: CGFloat = 1.0
     var body: some View {
-            ZStack{
+        NavigationView {
+            ZStack {
                 Image("IMG_3451")
                     .resizable()
                     .aspectRatio(contentMode: .fill)
                     .ignoresSafeArea()
-                VStack{
-                    Button(action:{
+                
+                VStack {
+                    Button(action: {
                         print("play button pressed")
-                    }){
-                       ( Image(systemName: "play.circle.fill")
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width:100, height: 100)
-                            .ignoresSafeArea())
-                            
-                    }
-                    .onTapGesture {
-                        withAnimation(.easeInOut(duration: 0.2).delay(0.2)) {
-                            self.scale = 1.0
+                    }) {
+                        NavigationLink(destination: LevelsPage()) {
+                            Image(systemName: "play.circle.fill")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .padding(.bottom,-50)
                         }
                     }
+                    
                     Text("BUBS")
                         .font(.system(size: 70, weight: .bold, design: .rounded))
                         .italic()
                         .foregroundStyle(.blue)
-                        .shadow(color: .cyan, radius: 2, x:10, y:10)
+                        .shadow(color: .cyan, radius: 2, x: 10, y: 10)
                         .rotationEffect(.degrees(10))
-                        .padding(.top,-240)
+                        .padding(.top, -190)
                 }
             }
-                .ignoresSafeArea()
-            }
         }
+    }
+}
+
 #Preview {
     HomePage()
 }
